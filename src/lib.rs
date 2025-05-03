d8214098b801167edae6be818c679ae00ab44484
Yousuf Ali <aliyyousuf@gmail.com>
2025-05-03 21:11:48 +0600

    Added test function for multiplication

--- src/lib.rs
+++ src/lib.rs
@@ -19,4 +19,10 @@ mod tests {
         let result = add(2, 2);
         assert_eq!(result, 4);
     }
+
+    #[test]
+    fn it_works2() {
+        let result = multiplication(2, 3);
+        assert_eq!(result, 6)
+    }
 }
